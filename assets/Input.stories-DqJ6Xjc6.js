8df01de558cc26ffec02cd2fd65331ac584bac03
thelostsoul88 <thelostsoul88@users.noreply.github.com>
2025-10-29 12:36:21 +0000

    deploy: 9783c40515225912023378411fd53206f6c2413c

---
+++ assets/Input.stories-DqJ6Xjc6.js
@@ -0,0 +1,27 @@
+import{I as C}from"./Input-C_U_KmhK.js";import"./iframe-D--TWi2A.js";import"./preload-helper-C1FmrZbK.js";const P={title:"Input/Input",component:C,args:{placeholder:"Enter value"}},e={args:{type:"text",label:"Text"}},r={args:{type:"password",label:"Password"}},a={args:{type:"number",label:"Number"}},s={args:{clearable:!0,label:"Clearable"}},o={args:{label:"Disabled",disabled:!0,value:"Readonly"}};var t,l,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    type: "text",
+    label: "Text"
+  }
+}`,...(n=(l=e.parameters)==null?void 0:l.docs)==null?void 0:n.source}}};var c,p,d;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    type: "password",
+    label: "Password"
+  }
+}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var m,u,b;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    type: "number",
+    label: "Number"
+  }
+}`,...(b=(u=a.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};var i,g,x;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    clearable: true,
+    label: "Clearable"
+  }
+}`,...(x=(g=s.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};var y,w,S;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    label: "Disabled",
+    disabled: true,
+    value: "Readonly"
+  }
+}`,...(S=(w=o.parameters)==null?void 0:w.docs)==null?void 0:S.source}}};const T=["Text","Password","Number","Clearable","Disabled"];export{s as Clearable,o as Disabled,a as Number,r as Password,e as Text,T as __namedExportsOrder,P as default};
